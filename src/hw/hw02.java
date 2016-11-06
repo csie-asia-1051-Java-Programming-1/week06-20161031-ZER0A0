5000030c82133af7d0849bd04576018f898dca94
ZER0A0 <105021041@live.asia.edu.tw>
2016-11-06 18:16:01 +0800

    WEEK06 HW 繳交

--- src/hw/hw02.java
+++ src/hw/hw02.java
@@ -1,7 +1,13 @@
 package hw;
 /*
+ * Topic: 讓使用者輸入性別與身高，透過函數呼叫，依據男女不同, 
+ * 幫她(他)計算並輸出其標準體重
+ *  ((1)男：(身高-170)*0.6+62 , (2)女：(身高-158)*0.5+52)。
+ *  函數算出結果後要回傳至主程式再印出結果。
+ *  例如:輸入 1 170 則輸出 62.0, 輸入 2 165 則輸出 55.5 
 
  * Date: 2016/10/31
+ * Author: 105021041 楊建宸
  */
 
 import java.util.Scanner;
@@ -11,6 +17,7 @@ public class hw02 {
 		// TODO Auto-generated method stub
 		Scanner scn = new Scanner(System.in);
 		
+		System.out.print("男輸入1,女輸入2：");
 		int n = scn.nextInt();
 		double m = scn.nextDouble();
 		GG(n , m);
